0ef304a9bd72258a45a80a5a4c94eff545a7cfae
amotin <amotin>
2010-10-12 11:20:15 +0000

    Fix some possible memory leaks.

--- src/radius.c
+++ src/radius.c
@@ -1,7 +1,7 @@
 /*
  * See ``COPYRIGHT.mpd''
  *
- * $Id: radius.c,v 1.151 2010/08/31 21:51:39 amotin Exp $
+ * $Id: radius.c,v 1.152 2010/10/12 11:20:15 amotin Exp $
  *
  */
 
@@ -1847,6 +1847,7 @@ RadiusGetParams(AuthData auth, int eap_proxy)
 	    if (acl1 == NULL) {
 	      Log(LG_ERR, ("[%s] RADIUS: Incorrect acl!",
 		auth->info.lnkname));
+	      free(acl);
 	      break;
 	    }
 	    
