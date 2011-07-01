cb5c433e0db5428ebd74de8bb13f5900feb1eecf
dmitryluhtionov <dmitryluhtionov>
2011-07-01 06:51:48 +0000

    Fix bug 3175664

--- src/radius.c
+++ src/radius.c
@@ -1,7 +1,7 @@
 /*
  * See ``COPYRIGHT.mpd''
  *
- * $Id: radius.c,v 1.154 2011/06/30 14:22:56 dmitryluhtionov Exp $
+ * $Id: radius.c,v 1.155 2011/07/01 06:51:48 dmitryluhtionov Exp $
  *
  */
 
@@ -1569,7 +1569,7 @@ RadiusGetParams(AuthData auth, int eap_proxy)
       case RAD_FRAMED_POOL:
 	tmpval = rad_cvt_string(data, len);
 	/* copy it into the persistent data struct */
-	strcpy(auth->params.ippool, tmpval);
+	strlcpy(auth->params.ippool, tmpval, sizeof(auth->params.ippool));
 	free(tmpval);
 	Log(LG_RADIUS2, ("[%s] RADIUS: Get RAD_FRAMED_POOL: %s ",
 	  auth->info.lnkname, auth->params.ippool));
